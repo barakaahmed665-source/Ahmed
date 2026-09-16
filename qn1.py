71a7e1b43998522b04aea78cd2eacc6e9494f826
Baraka08 <barakaahmed665@gmail.com>
2026-09-16 08:26:27 -0700

    Add files via upload

---
+++ qn1.py
@@ -0,0 +1,56 @@
+class Student:
+    pass
+#Creating the first student object with it's values
+student1 = Student()
+student1.student_ID ="B100"
+student1.name = "Ahmed Mubarak"
+student1.age = 22
+student1.gender = "Male"
+student1.programe ="Business Administration"
+student1.year_of_study = 2
+
+#Student 2 as an object
+student2 = Student()
+student2.student_ID ="B200"
+student2.name = "Ahmed Mubarak"
+student2.age = 22
+student2.gender = "Male"
+student2.programe ="Business Administration"
+student2.year_of_study = 2
+
+#Student 3 as an object 
+student3 = Student()
+student3.student_ID ="B300"
+student3.name = "Ahmed Mubarak"
+student3.age = 22
+student3.gender = "Male"
+student3.programe ="Business Administration"
+student3.year_of_study = 2
+
+#Displaying their results
+print("\n   Student 1 Details ")
+print("Student1 ID :",student1.student_ID)
+print("Student1 name :",student1.name)
+print("Student1 age :",student1.age)
+print("Student1 gender :",student1.gender)
+print("Student1 Programe :",student1.programe)
+print("Student1 year of study :",student1.year_of_study)
+
+#Displaying Student2 Results
+print("\n   Student 2 Details ")
+print("Student2 ID :",student2.student_ID)
+print("Student2 name :",student2.name)
+print("Student2 age :",student2.age)
+print("Student2 gender :",student2.gender)
+print("Student2 Programe :",student2.programe)
+print("Student2 year of study :",student2.year_of_study)
+
+
+#Displaying Student 3 Results
+print("\n   Student 1 Details ")
+print("Student3 ID :",student3.student_ID)
+print("Student3 name :",student3.name)
+print("Student3 age :",student3.age)
+print("Student3 gender :",student3.gender)
+print("Student3 Programe :",student3.programe)
+print("Student3 year of study :",student3.year_of_study)
