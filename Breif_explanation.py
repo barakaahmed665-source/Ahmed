6fb40a25241d20ad7a4d12bebc9a446d35a47605
Baraka08 <barakaahmed665@gmail.com>
2026-09-16 07:44:56 -0700

    Add files via upload

---
+++ Breif_explanation.py
@@ -0,0 +1,22 @@
+# Classes are blue prints used to create objets such as Student in Object Oriented Programming
+
+#Objects are the actual instances of the class. They are created using the class construct
+# or. The class constructor is a special method that is called when an object is created.
+# It is used to initialize the object's attributes.
+
+#Attributes are the data members of the class. They are used to store the state of the obj
+# ect.
+
+#Methods are functions that are defined inside the class. They are used to define the beha
+# vior of the object.
+
+#Constructors are special methods that are called when an object is created. They are used
+# to initialize the object's attributes.
+
+#Encapsulation is the process of hiding the internal details of an object from the outside
+# world. It is achieved by using private attributes and methods.
+
+
+
+
+    
